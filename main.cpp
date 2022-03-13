863dfc956cfa6d9ce849d56c87f99eb79e15e142
sacredLamp <aidansommeriii@gmail.com>
2022-03-13 17:46:55 -0400

    whatever

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-//THIS IS IT
+#include <iostream>
+#include <cmath>
+
+
+int main(){
+   using namespace std;
+   string multi;
+   multi = "";
+   int p1;
+   int p2;
+   int comp1;
+   cout << "Are you playing against a friend? yes or no: "; cin >> multi;
+   while(!(multi != "no") && !(multi != "yes"))
+   {
+      cout << "Please enter yes or no: "; cin >> multi;
+   }
+   cout << multi;
+}
